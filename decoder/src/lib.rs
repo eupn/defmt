f27cf783fcf582f6553b3d7c3f5d75047570ce4f
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2021-01-27 15:05:49 +0100

    Use type hint precedence for Uxx values
    This change checks the inner and outer type hint of nested `Uxx` values.
    
    For example:
    
    ```rust
    struct S { x: u8 }
    
    // derive expands to
    impl defmt::Format for S {
      fn format(&self, f: defmt::Formatter) {
        write!(f, "S {{ x: {=u8:?} }}", self.x);
      }
    }
    ```
    
    where inner value `x` is of type `u8` with display hint `Debug`. When
    this struct is formated using an outer display hint then the outer hint
    will be used.
    
    ```rust
    info!("{:x}", S { x: 42 });
    ```
    
    renders as: `INFO S { x: 0x2a }` due to the `:x` (hex) display hint.
    
    On the other hand if the inner type is one of `:x` (hex), `:b` (binary)
    or `:a` (ascii), the outer display hint will be ignored.
    
    ```rust
    impl defmt::Format for S {
      fn format(&self, f: defmt::Formatter) {
        write!(f, "S {{ x: {=u8:b} }}", self.x);
      }
    }
    ```
    
    The outer display hint (`:x`) in
    
    ```rust
    info!("{:x}", S { x: 42 });
    ```
    
    will be ignored, the output is rendered with inner display hint `:b` as `INFO S { x: 0b101010 }`
    
    * remove `seen_hits` property from `Arg::Format` variant

--- decoder/src/lib.rs
+++ decoder/src/lib.rs
@@ -589,7 +589,7 @@ mod tests {
     }
 
     #[test]
-    fn display_use_inner_hex_hint() {
+    fn display_use_inner_type_hint() {
         let mut entries = BTreeMap::new();
 
         entries.insert(
@@ -616,7 +616,7 @@ mod tests {
             42,  // value
         ];
 
-        let frame = super::decode(&bytes, &table).unwrap().0;
+        let frame = table.decode(&bytes).unwrap().0;
         assert_eq!(
             frame.display(false).to_string(),
             "0.000002 INFO x=S { x: 0x2a }",
@@ -651,7 +651,7 @@ mod tests {
             42,  // value
         ];
 
-        let frame = super::decode(&bytes, &table).unwrap().0;
+        let frame = table.decode(&bytes).unwrap().0;
         assert_eq!(
             frame.display(false).to_string(),
             "0.000002 INFO x=S { x: 0b101010 }",
