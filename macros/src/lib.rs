142e956bfd6b1d9df7fb933a77c65f1cc9991053
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2021-02-04 12:18:11 +0100

    Set :? type hint to inner fields
    Instead of applying the Debug type hint `:?` to all inner fields.
    
    * add test to check struct with generic type
    * update test output

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -410,11 +410,7 @@ fn fields(
                         "?".to_string()
                     });
                     if let Some(ident) = f.ident.as_ref() {
-                        if ty == "str" {
-                            core::write!(format, "{}: {{={}:?}}", ident, ty).ok();
-                        } else {
-                            core::write!(format, "{}: {{={}}}", ident, ty).ok();
-                        }
+                        core::write!(format, "{}: {{={}:?}}", ident, ty).ok();
 
                         if ty == "?" {
                             list.push(quote!(f.inner.fmt(#ident, false)));
